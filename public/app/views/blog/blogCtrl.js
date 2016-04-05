53c6a34ac43e309d3c5b7a2f17c6c72b4e8e73bc
Nathan Allen <nlallenster@gmail.com>
2016-04-05 17:18:46 -0500

    updated media queries and blog styling

--- public/app/views/blog/blogCtrl.js
+++ public/app/views/blog/blogCtrl.js
@@ -7,12 +7,6 @@ var syncArray = $firebaseArray(firebaseObj);
 $scope.articles = syncArray;
 
 
-
-// $scope.trim = function(text) {
-//   return text.replace("/\n\n/g", "<br />");
-// }
-
-
 $scope.editPost = function(id) {
   var firebaseObj = new Firebase("https://peisercpa.firebaseio.com/Articles/" + id);
 
@@ -60,5 +54,94 @@ $scope.deletePost = function() {
     article.remove(onComplete);
 
 }
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+      $('#slideIn-mobile').css('display', 'block');
+      $('#slideIn-mobile').show();
+    } else {
+      $('#slideIn-mobile').hide();
+    }
+  })
+})
+
+  var modal = (function(){                          // Declare modal object
+
+    var $modal = $('<div class="homeModal"/>'),         // Create markup for modal
+        $content = $('<div class="homeModal-content"/>'),
+        $close = $('<button role="button" class="homeModal-close">Close</button>');
+        $overlay = $('<div class="overlay"/>');     // Create overlay element
+
+    $modal.append($content, $close);                // Add close button to modal
+
+    $close.click(function(e){                       // If user clicks on close
+      e.preventDefault();                           // Prevent link behavior
+      modal.close();                                // Close the modal window
+    });
+
+    $(document).click(function(){
+      modal.close();
+    });
+    $(document).keyup(function(e){
+      if (e.keyCode === 27) {
+        modal.close();
+      }
+    });
+    $('.share').click(function(e){
+      e.stopPropagation();
+    });
+
+
+
+    return {                                         // Add code to modal
+      center: function () {                          // Define center() method
+        // Calculate distance from top and left of window to center the modal
+        var top = Math.max($(window).height() - $modal.outerHeight(), 0) / 2,
+            left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
+        $modal.css({                                 // Set CSS for the modal
+          top:top + $(window).scrollTop(),           // Center vertically
+          left:left + $(window).scrollLeft()         // Center horizontally
+        });
+      },
+      open: function (settings) {                    // Define open() method
+        $content.empty().append(settings.content);   // Set new content of modal
+
+        $modal.css({                                 // Set modal dimensions
+          width: settings.width || 'auto',           // Set width
+          height: settings.height || 'auto'          // Set height
+        }).appendTo('body');                         // Add it to the page
+        $('#blogWrapper').css('opacity', '0.2'); //set background opacity to 0.2
+
+        modal.center();                              // Call center() method
+        $(window).on('resize', modal.center);        // Call it if window resized
+        $('.modal').after($overlay);
+      },
+      close: function () {                           // Define close() method
+          $content.empty();                          // Remove content from modal
+          $modal.detach();                           // Remove modal from page
+          $overlay.detach();                         // Remove the overlay
+          $(window).off('resize', modal.center);     // Remove event handler
+          $('#blogWrapper').css('opacity', '1'); //set background opacity back to 1
+      }
+    };
+  }());
+
+
+
+  // Modal Init
+
+  (function() {
+    var $content = $('#contact-options').detach(); // Remove modal from page
+
+    $('.share').on('click', function() { // Click handler to open modal
+      modal.open({
+        content: $content,
+        width: 340,
+        height: 400
+      });
+    });
+  }());
+
 
 }]);
